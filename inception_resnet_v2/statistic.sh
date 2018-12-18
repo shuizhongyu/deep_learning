218a4263af403b35dc86c5c2abf63c6abd7909d3
ljx <jiaxinustc@gmail.com>
2018-12-18 14:36:19 +0800

    update statistic.sh,add four class:line3

--- inception_resnet_v2/statistic.sh
+++ inception_resnet_v2/statistic.sh
@@ -177,9 +177,104 @@ function precision_recall3()
     #echo -n precision=`echo "scale=4;$OO/($OY+$ON+$OO)*100"|bc|tr -d '0'`%" "
     #echo recall=`echo "scale=4;$OO/($NO+$YO+$OO)*100"|bc|tr -d '0'`%
 }
+#4类版本
+function precision_recall4()
+{
+    file=$1
+    yes_file=$2
+    no1_file=$3
+    no2_file=$4
+    other_file=$5
+    row_yes=$6
+    row_no1=$7
+    row_no2=$8
+    row_other=$9
+    #echo $file $yes_file $no1_file $no2_file $other_file $row_yes $row_no1 $row_no2 $row_other
+
+
+    cat $file \
+    |awk '$'$row_yes'>$'$row_no1'&&$'$row_yes'>$'$row_no2'&&$'$row_yes'>$'$row_other'{printf("%s \n",$1)}'\
+    |awk -F"/" '{print $NF}'\
+    |tr -d "'" |tr -d " "\
+    |sort >temp_yes
+
+    cat $file \
+    |awk '$'$row_no1'>$'$row_yes'&&$'$row_no1'>$'$row_no2'&&$'$row_no1'>$'$row_other'{printf("%s \n",$1)}'\
+    |awk -F"/" '{print $NF}'\
+    |tr -d "'" |tr -d " "\
+    |sort >temp_no1
+
+    cat $file \
+    |awk '$'$row_no2'>$'$row_yes'&&$'$row_no2'>$'$row_no1'&&$'$row_no2'>$'$row_other'{printf("%s \n",$1)}'\
+    |awk -F"/" '{print $NF}'\
+    |tr -d "'" |tr -d " "\
+    |sort >temp_no2
+
+    cat $file \
+    |awk '$'$row_other'>$'$row_yes'&&$'$row_other'>$'$row_no1'&&$'$row_other'>$'$row_no2'{printf("%s \n",$1)}'\
+    |awk -F"/" '{print $NF}'\
+    |tr -d "'" |tr -d " "\
+    |sort >temp_other
+
+    #第一位代表标记类别，第二位代表实际类别
+    #echo $yes_file $no_file $other_file
+    YY=`comm -12 temp_yes $yes_file|wc -l`
+    YN1=`comm -12 temp_yes $no1_file|wc -l`
+    YN2=`comm -12 temp_yes $no2_file|wc -l`
+    YO=`comm -12 temp_yes $other_file|wc -l`
+
+    N1Y=`comm -12 temp_no1 $yes_file|wc -l`
+    N1N1=`comm -12 temp_no1 $no1_file|wc -l`
+    N1N2=`comm -12 temp_no1 $no2_file|wc -l`
+    N1O=`comm -12 temp_no1 $other_file|wc -l`
+
+    N2Y=`comm -12 temp_no2 $yes_file|wc -l`
+    N2N1=`comm -12 temp_no2 $no1_file|wc -l`
+    N2N2=`comm -12 temp_no2 $no2_file|wc -l`
+    N2O=`comm -12 temp_no2 $other_file|wc -l`
+
+    OY=`comm -12 temp_other $yes_file|wc -l`
+    ON1=`comm -12 temp_other $no1_file|wc -l`
+    ON2=`comm -12 temp_other $no2_file|wc -l`
+    OO=`comm -12 temp_other $other_file|wc -l`
+
+
+    #rm temp_yes temp_no1 temp_no2 temp_other
+
+    echo -e  "--  yes  no1 no2 other \nyes $YY $YN1 $YN2 $YO \nno1 $N1Y $N1N1 $N1N2 $N1O \n no2 $N2Y $N2N1 $N2N2 $N2O \nother $OY $ON1 $ON2 $OO" >temp
+    cat temp | column -t
+    rm temp
 
 
 
+    echo ""
+    echo yes:
+    echo 1|awk '{printf("precision=%2.2f%%  "),'$YY'/('$YY'+'$YN1'+'$YN2'+'$YO')*100}'
+    echo 1|awk '{printf("recall=%2.2f%%  \n"),'$YY'/('$YY'+'$N1Y'+'$N2Y'+'$OY')*100}'
+
+    echo no1:
+    echo 1|awk '{printf("precision=%2.2f%%  "),'$N1N1'/('$N1Y'+'$N1N1'+'$N1N2'+'$N1O')*100}'
+    echo 1|awk '{printf("recall=%2.2f%%  \n"),'$N1N1'/('$YN1'+'$N1N1'+'$N2N1'+'$ON1')*100}'
+
+    echo no2:
+    echo 1|awk '{printf("precision=%2.2f%%  "),'$N2N2'/('$N2Y'+'$N2N1'+'$N2N2'+'$N2O')*100}'
+    echo 1|awk '{printf("recall=%2.2f%%  \n"),'$N2N2'/('$YN2'+'$N1N2'+'$N2N2'+'$ON2')*100}'
+
+    echo other:
+    echo 1|awk '{printf("precision=%2.2f%%  "),'$OO'/('$OY'+'$ON1'+'$ON2'+'$OO')*100}'
+    echo 1|awk '{printf("recall=%2.2f%%  \n"),'$OO'/('$YO'+'$N1O'+'$N2O'+'$OO')*100}'
+
+    echo ""
+}
+
+#statistic line_3.csv 3
+#statistic line_3.csv 5
+#statistic line_3.csv 7
+#statistic line_3.csv 9
+#precision_recall4 line_3.csv line3/yes line3/no1 line3/no2 line3/other 5 7 9 3
+#precision_recall4 line_resnet3.csv line3/yes line3/no1 line3/no2 line3/other 5 7 9 3
+
+#exit
 
 echo ""
 
@@ -208,6 +303,23 @@ case $1 in
             precision_recall3 line.csv ../test2_result/line/line_yes ../test2_result/line/line_no ../test2_result/line/line_other 5 3 7
         fi
         ;;
+    line3):
+        if [ $2 -eq 1 ];then
+            statistic line_3.csv 5
+        else
+            echo line3:
+            precision_recall4 line_3.csv line3/yes line3/no1 line3/no2 line3/other 5 7 9 3
+        fi
+        ;;
+    line_resnet3):
+        if [ $2 -eq 1 ];then
+            statistic line_resnet3.csv 5
+        else
+            echo line_resnet3:
+            precision_recall4 line_resnet3.csv line3/yes line3/no1 line3/no2 line3/other 5 7 9 3
+        fi
+        ;;
+
     qr):
         if [ $2 -eq 1 ];then
             statistic qr.csv 5
@@ -227,6 +339,7 @@ case $1 in
     *):
         echo usage:./statistic.sh box/line/hole/hat/qr 1/2
 esac
+
 
 #列是合格的那列
 #statistic box.csv 3
